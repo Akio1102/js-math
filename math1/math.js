f294283f110c91b22929d582f6acdc0f792f9983
Akio <cristian.akio1102@gmail.com>
2022-08-10 19:43:51 -0500

    calcular Circulo con Math.PI

--- math1/math.js
+++ math1/math.js
@@ -43,3 +43,31 @@ console.log({
   areaTriangulo,
 });
 console.groupEnd("Triangulo");
+
+console.group("Circulo");
+
+const radioCirculo = 3;
+const diametroCirculo = radioCirculo * 2;
+
+const PI = 3.1415;
+
+const circunferencia = diametroCirculo * PI;
+const areaCirculo = radioCirculo ** 2 * PI;
+
+console.log({
+  radioCirculo,
+  diametroCirculo,
+  PI,
+  circunferencia,
+  areaCirculo,
+});
+
+function calcularCirculo(radio) {
+  const diametro = radio * 2;
+  const radioAlCirculo = Math.pow(radio, 2);
+  return {
+    circunferencia: diametro * Math.PI,
+    area: radioAlCirculo * Math.PI,
+  };
+}
+console.groupEnd("Circulo");
